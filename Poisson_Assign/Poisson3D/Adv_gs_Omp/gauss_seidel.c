4bcac066418ed76248e2e0da20908639cdb102f9
BernhardJonsson <bernton96@gmail.com>
2022-01-13 16:35:08 +0100

    Working on adv openmp

--- Poisson_Assign/Poisson3D/Adv_gs_Omp/gauss_seidel.c
+++ Poisson_Assign/Poisson3D/Adv_gs_Omp/gauss_seidel.c
@@ -10,29 +10,34 @@ int gauss_seidel(double ***f, double ***u, double ***u_old, int N, int k_max, do
 	double delta = (1.0/(double)N)*(1.0/(double)N);
 	// var for calc frobenius norm
 	double sum, val=0.0, tmp=0.0;
-    
-    while (d > threshold && counter < k_max) {
-    	sum = 0.0;
+
+	#pragma omp parallel default(none) \
+	 firstprivate(k_max) private(i,j,k,tmp,val) \
+	 shared(N,u,delta,f,counter,d) reduction(+:sum)
+	for (counter = 0; counter < k_max; counter++) {
+		#pragma omp critical    	
+		sum = 0.0;
 		// update u
-        #pragma omp parallel for ordered(2) default(none) private(i,j,k,tmp,val) shared(N,u,delta,f) schedule(static,1) reduction(+:sum)
+		#pragma omp for ordered(2) schedule(static,1)
 		for (i = 1; i < (N + 1); i++)
     	{
         	for (j = 1; j < (N + 1); j++)
-        	{
-                #pragma omp ordered depend(sink:i-1,j) depend(sink:i,j-1)
+       		{
+				#pragma omp ordered depend(sink:i-1,j) depend(sink:i,j-1)
             	for (k = 1; k < (N + 1); k++)
-            	{
+       	    	{
 					tmp = u[i][j][k];		
                 	u[i][j][k] = (1.0/6.0)*(u[i-1][j][k] + u[i+1][j][k] + u[i][j-1][k] + u[i][j+1][k] + u[i][j][k-1] + u[i][j][k+1] + delta*f[i][j][k]);
             		val = u[i][j][k] - tmp;
 					sum += val*val;				
 				}
-                #pragma omp ordered depend(source)
-        	}
-    	}
-        d = sqrt(sum); 
-		counter = counter + 1;
-    }
+               	#pragma omp ordered depend(source)
+       		}
+   		}
+		#pragma omp critical
+		d = sqrt(sum); 
+	}
+
     return counter;
 }
 
